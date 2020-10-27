a11dcde775de4856a36601fe2df1c5704056f13f
Jackson Wiese <jwiese5363@conestogac.on.ca>
2020-10-27 14:07:54 -0400

    First commit

---
+++ Group5_GameStore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Group5_GameStore.MvcApplication" Language="C#" %>
---
+++ Group5_GameStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Group5_GameStore.Startup))]
+namespace Group5_GameStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
